96854fc293131f34268571c23e4c289b6229af2b
David Alejandro Sanchez Polo <65139707+lasagna0@users.noreply.github.com>
2023-05-01 23:15:49 -0500

    grid_response=AgGrid(barriossincol[{'BARRIO', 'LOCALIDAD', 'ipm', 'nearest_parque', 'nearest_parque_distance'}].sort_values(by='ipm',ascending=False), gridOptions=gridOptions,

--- app.py
+++ app.py
@@ -584,7 +584,7 @@ def app_layout():
     gb.configure_pagination(paginationAutoPageSize=True)
     gb.configure_side_bar()
     gridOptions=gb.build()
-    grid_response=AgGrid(barriossincol[{'BARRIO', 'LOCALIDAD', 'ipm', 'nearest_parque', 'nearest_parque_distance'}].sort_values(by='ipm',ascending=False), gridOptions=gridOptions,
+    grid_response=AgGrid(barriossincol[['BARRIO', 'LOCALIDAD', 'ipm', 'nearest_parque', 'nearest_parque_distance']].sort_values(by='ipm',ascending=False), gridOptions=gridOptions,
                         height=400, 
                         width='100%',
                         data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
